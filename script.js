e5680daf82c35164f41584bb121b5f73f83968be
niks-developer <112979258+niks-developer@users.noreply.github.com>
2023-05-15 17:52:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getIP(){
     getInfo(result.city);
 }
 
-getIP();
+
 
 
 const input = document.querySelector("#input"); // доступ к полю ввода
@@ -97,5 +97,5 @@ function getOurDate() { // описание функции получить Да
     
 }
 
-
+getIP();
 
